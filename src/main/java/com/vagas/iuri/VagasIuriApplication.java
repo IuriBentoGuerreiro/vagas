8071bcfddfa36480721068abcf2ff5153f9eb3c7
iuri <bentoiuri@gmail.com>
2024-11-19 16:22:01 -0300

    renomeia application

--- src/main/java/com/vagas/iuri/VagasIuriApplication.java
+++ src/main/java/com/vagas/iuri/VagasIuriApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RemotinhoApplication {
+public class VagasIuriApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RemotinhoApplication.class, args);
+		SpringApplication.run(VagasIuriApplication.class, args);
 	}
 
 }
